b4d0e3a79d1db2ac3f1daa4bc6dc109a64f124d6
nuits.jp <nuits.jp@live.jp>
2017-08-30 11:21:36 +0900

    Add Projects.

---
+++ NuGet/package.cmd
@@ -0,0 +1,1 @@
+NuGet.exe pack "..\Source\Nuits.System.Device.Location\Nuits.System.Device.Location\Nuits.System.Device.Location.csproj" -Prop Configuration=Release -Symbol -OutputDirectory "..\Release"
